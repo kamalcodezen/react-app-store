a7c700c59317fd739a55731313a9c69e41f2c95d
kamalcodezen <kamaluddin7908@gmail.com>
2026-07-05 01:59:30 +0530

    setup : projects initialize with all resources

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <h1 className="text-4xl font-bold bg-rose-500">Hello World</h1>
+  </StrictMode>,
+);
